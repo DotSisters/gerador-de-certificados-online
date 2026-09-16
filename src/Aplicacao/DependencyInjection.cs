ced83234e8be68298766f7bb8937fd833adbbe65
Natalia Bortoli Vieira <nataliabortolivieira@gmail.com>
2026-09-16 23:49:44 +0200

    first commit

---
+++ src/Aplicacao/DependencyInjection.cs
@@ -0,0 +1,67 @@
+// using GeradorDeCertificados.Aplicacao.Modulos.Certificados.Mensageria;
+using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GeradorDeCertificados.Aplicacao;
+
+public static class DependencyInjection
+{
+    public static void AddApplicationServices(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
+    {
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+        });
+
+        var rabbitMqConnectionString = configuration.GetConnectionString("RabbitMq")
+            ?? throw new InvalidOperationException("A ConnectionString \"RabbitMq\" não foi configurada");
+
+        services.AddMassTransit(config =>
+        {
+            config.SetKebabCaseEndpointNameFormatter();
+
+            // Configura a injeção dos Consumers
+            // config.AddConsumer<GerarCertificadosConsumer>();
+            // config.AddConsumer<GerarZipConsumer>();
+
+            config.UsingRabbitMq((context, rabbitMq) =>
+            {
+                rabbitMq.Host(new Uri(rabbitMqConnectionString));
+
+                // rabbitMq.ReceiveEndpoint("certificados-solicitados", endpoint =>
+                // {
+                //     endpoint.PrefetchCount = 4; // Quantas mensagens o RabbitMQ deve carregar adiantado
+                //     endpoint.ConcurrentMessageLimit = 2; // Quantos consumers serão instanciados em paralelo
+                //     endpoint.UseMessageRetry(DefaultMessageRetryIntervals); // Quantas re-tentativas serão feitas e o intervalo entre elas
+                //
+                //     endpoint.ConfigureConsumer<GerarCertificadosConsumer>(context);
+                // });
+
+                // rabbitMq.ReceiveEndpoint("certificados-zip", endpoint =>
+                // {
+                //     endpoint.PrefetchCount = 4;
+                //     endpoint.ConcurrentMessageLimit = 2;
+                //     endpoint.UseMessageRetry(DefaultMessageRetryIntervals);
+                //
+                //     endpoint.ConfigureConsumer<GerarZipConsumer>(context);
+                // });
+            });
+        });
+
+        services.Configure<MassTransitHostOptions>(options =>
+        {
+            options.WaitUntilStarted = true;
+            options.StartTimeout = TimeSpan.FromSeconds(30);
+        });
+    }
+
+    private static void DefaultMessageRetryIntervals(IRetryConfigurator retry) => retry.Intervals(
+        TimeSpan.FromSeconds(1),
+        TimeSpan.FromSeconds(5),
+        TimeSpan.FromSeconds(15)
+    );
+}
